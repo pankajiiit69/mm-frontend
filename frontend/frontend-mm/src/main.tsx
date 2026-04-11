276244597ae62714014c77f1fa5b02be65351e42
pankajiiit69 <pankaj.iiit69@gmail.com>
2026-04-11 17:45:26 +0530

    revert the Router chnages

--- frontend/frontend-mm/src/main.tsx
+++ frontend/frontend-mm/src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import { AuthProvider } from '@fruzoos/auth-core'
 import { authClient } from './api/authClient'
@@ -13,14 +13,14 @@ const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID ?? ''
 const hasGoogleClientId = Boolean(googleClientId.trim())
 
 const appTree = (
-  <HashRouter>
+  <BrowserRouter>
     <AuthProvider authClient={authClient} storage={storage} storageKey={AUTH_KEY}>
       <ToastProvider>
         <App />
         <ToastContainer />
       </ToastProvider>
     </AuthProvider>
-  </HashRouter>
+  </BrowserRouter>
 )
 
 createRoot(document.getElementById('root')!).render(
